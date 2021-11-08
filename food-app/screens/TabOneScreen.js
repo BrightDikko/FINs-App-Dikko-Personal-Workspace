90df8b9e581512a152ff73d8d20844f7478e7bc5
Jack Gordley <jgordley99@gmail.com>
2021-11-08 18:36:42 -0500

    Initilizing React Native App

---
+++ food-app/screens/TabOneScreen.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+import { firebase } from '../src/firebase/config'
+
+import { Button, Center, NativeBaseProvider } from "native-base";
+
+
+export const storeHighScore = (score) => {
+  // firebase.auth().createUserWithEmailAndPassword("jgordley99@gmail.com", "screwit").then((response) => {
+  //   const uid = response.user.uid;
+  //   const data = {
+  //     id: uid,
+  //     email,
+  //     fullName
+  //   };
+  //   console.log(data);
+  // });
+  firebase.database().ref('users/jack').set({
+    highscore: score
+  })
+}
+
+export const Example = () => {
+    return (
+      <>
+        <Button onPress={() => storeHighScore(10)}>Primary</Button>
+      </>
+    )
+  }
+
+  export default function TabOneScreen(navigation) {
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Home Screen</Text>
+
+        <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <NativeBaseProvider>
+          <Example />
+        </NativeBaseProvider>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    separator: {
+      marginVertical: 30,
+      height: 1,
+      width: '80%',
+    },
+  });
---
+++ food-app/screens/TabTwoScreen.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import ListWithImages from '../components/ListWithImages';
+
+export default function TabTwoScreen() {
+  return (
+    <ListWithImages />
+  );
+}
