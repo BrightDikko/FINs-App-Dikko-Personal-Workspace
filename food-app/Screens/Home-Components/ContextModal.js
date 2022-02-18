86d3181a298a749ff432d05d87d9fa0d82c76e21
Meredith Heller <meredith.heller.vabzby@statefarm.com>
2022-02-18 12:00:03 -0500

    working UI

--- food-app/Screens/Home-Components/ContextModal.js
+++ food-app/Screens/Home-Components/ContextModal.js
@@ -1,15 +1,56 @@
-import React, { useState } from 'react';
-import { Alert, Modal, Style, StyleSheet, Text, Pressable, View, SafeAreaView, ScrollView, StatusBar, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Alert, Modal, Style, StyleSheet, Text, Pressable, View, SafeAreaView, ScrollView, StatusBar, TextInput, TouchableOpacity } from 'react-native';
+import { ButtonGroup, CheckBox } from 'react-native-elements';
 
 
 
-
-const ContextModal = ( displayModal, isRegisteredUser ) => {
+const ContextModal = ( displayModal, isRegisteredUser, navigation, route ) => {
 
     const [modalVisible, setModalVisible] = useState(true);
-    const [ budget, setBudget ] = useState(null);
-    const [ cuisine, setCuisine ] = useState([]);
-    const [ paymentMethod, setPaymentMethod ] = useState()
+    const [budget, setBudget ] = useState(null);
+    const [isItalianSelected, setItalianSelected] = useState(false);
+    const [isIndianSelected, setIndianSelected] = useState(false);
+    const [isMexicanSelected, setMexicanSelected] = useState(false);
+    const [isChineseSelected, setChineseSelected] = useState(false);
+    const [isAmericanSelected, setAmericanSelected] = useState(false);
+    const [isFrenchSelected, setFrenchSelected] = useState(false);
+    const [isMedSelected, setMedSelected] = useState(false);
+    const [isGreekSelected, setGreekSelected] = useState(false);
+    const [isJapaneseSelected, setJapaneseSelected] = useState(false);
+    // const [isAmericanSelected, setAmericanSelected] = useState(false);
+    // const [isFrenchSelected, setFrenchSelected] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    var paymentArray = ['PayPal', 'Credit', 'Debit'];
+    var paymentMethod;
+
+    useEffect(() => {
+      paymentMethod = paymentArray[selectedIndex];
+      console.log(selectedIndex)
+    }, [selectedIndex]);
+
+    var cuisinePreferences = {
+      isItalianSelected,
+      isIndianSelected,
+      isMexicanSelected,
+      isChineseSelected,
+      isAmericanSelected,
+      isFrenchSelected,
+      isMedSelected,
+      isGreekSelected,
+      isJapaneseSelected
+    }
+
+
+    function handleSessionInfoSubmit() {
+      const listInfo = {
+        cuisinePreferences, 
+        budget,
+        paymentMethod
+      }
+
+      // route.params.add
+    }
     
     return (
       <View style={styles.centeredView}>
@@ -29,24 +70,84 @@ const ContextModal = ( displayModal, isRegisteredUser ) => {
                   Enter List Constraints
                 </Text>
               </View>
-              <ScrollView>
-                <View>
-                  <TextInput
-                    style={styles.budgetInput}
-                    onChangeText={setBudget}
-                    value={budget}
-                    placeholder="Enter budget"
-                    keyboardType="numeric"
-                  />
-                </View>
-              </ScrollView>
+              <View style={{ flex: 5, alignItems: 'center' }}>
+                <ScrollView>
+                  <View style={styles.inputView}>
+                    <TextInput
+                      style={styles.TextInput}
+                      value={budget}
+                      placeholder="Enter budget"
+                      placeholderTextColor='#525252'
+                      keyboardType="numeric"
+                      onChangeText={(budget) => setBudget(budget)}
+                    />
+                  </View>
+                  <Text style={{ textAlign: 'center', fontSize: 14 }}>{'\n'}What cuisine would you like to shop for?</Text>
+                        <CheckBox
+                            title="Italian"
+                            checked={isItalianSelected}
+                            onPress={() => setItalianSelected(!isItalianSelected)}
+                        />
+                        <CheckBox
+                            title="Indian"
+                            checked={isIndianSelected}
+                            onPress={() => setIndianSelected(!isIndianSelected)}
+                        />
+                        <CheckBox
+                            title="Mexican"
+                            checked={isMexicanSelected}
+                            onPress={() => setMexicanSelected(!isMexicanSelected)}
+                        />
+                        <CheckBox
+                            title="American"
+                            checked={isAmericanSelected}
+                            onPress={() => setAmericanSelected(!isAmericanSelected)}
+                        />
+                        <CheckBox
+                            title="Chinese"
+                            checked={isChineseSelected}
+                            onPress={() => setChineseSelected(!isChineseSelected)}
+                        />
+                        <CheckBox
+                            title="French"
+                            checked={isFrenchSelected}
+                            onPress={() => setFrenchSelected(!isFrenchSelected)}
+                        />
+                        <CheckBox
+                            title="Mediterranean"
+                            checked={isMedSelected}
+                            onPress={() => setMedSelected(!isMedSelected)}
+                        />
+                        <CheckBox
+                            title="Greek"
+                            checked={isGreekSelected}
+                            onPress={() => setGreekSelected(!isGreekSelected)}
+                        />
+                        <CheckBox
+                            title="Japanese"
+                            checked={isJapaneseSelected}
+                            onPress={() => setJapaneseSelected(!isJapaneseSelected)}
+                        />
+                        <ButtonGroup
+                          style={{
+                            margin: 20
+                          }}
+                          buttons={paymentArray}
+                          selectedIndex={selectedIndex}
+                          onPress={(value) => {
+                            setSelectedIndex(value);
+                          }}
+                          containerStyle={{ marginBottom: 20 }}
+                          />
+                </ScrollView>
+              </View>
               <View style={styles.buttonView}>
-                <Pressable
-                  style={[styles.button, styles.buttonClose]}
+                <TouchableOpacity
+                  style={[styles.button, styles.loginBtn]}
                   onPress={() => setModalVisible(!modalVisible)}
                 >
-                  <Text style={styles.textStyle}>Enter</Text>
-                </Pressable>
+                  <Text style={styles.loginText}>Enter</Text>
+                </TouchableOpacity>
               </View>
             </View>
           </View>      
@@ -125,6 +226,33 @@ const ContextModal = ( displayModal, isRegisteredUser ) => {
       borderWidth: 1,
       padding: 10,
       borderRadius: 5,
+    },
+    loginBtn: {
+      width: '100%',
+      borderRadius: 25,
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 40,
+      backgroundColor: '#53B175',
+    },
+    loginText: {
+      color: '#FFFFFF'
+    },
+    TextInput: {
+      height: 50,
+      flex: 1,
+      width: '80%',
+      padding: 5,
+    },
+    inputView: {
+      backgroundColor: '#E6E6E6',
+      borderRadius: 20,
+      width: '100%',
+      height: 45,
+      marginBottom: 20,
+      alignItems: 'center',
+      justifyContent: 'center'
     },
   });
   
