d27ef6aed7abef8b0c22f67084987b0077ca8676
Meredith Heller <meredith.heller.vabzby@statefarm.com>
2022-03-04 13:26:48 -0500

    main nav

--- food-app/Screens/Navigation-Stacks/MainNav.js
+++ food-app/Screens/Navigation-Stacks/MainNav.js
@@ -1,62 +1,28 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import UserIn from '../UserInfo.js';
-
+import ProfileStack from './ProfileStack.js';
+import CartStack from './CartStack.js'
+// import HomeStack from './HomeStack.js'
 
 const Tab = createBottomTabNavigator();
 
-<Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+const ProfileDrawer = ({ navigation, route }) => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="HomeStack"
+                    component={HomeStack}
+                />
+                <Tab.Screen name="CartStack"
+                    component={CartStack}
+                />
+                <Tab.Screen name="ProfileStack"
+                    component={ProfileStack}
+                />
+            </Tab.Navigator >
+        </NavigationContainer>
+    )
+}
 
-            if (route.name === "Home") {
-              iconName = focused ? "home" : "home-outline";
-            } else if (route.name === "Settings") {
-              iconName = focused ? "settings" : "settings-outline";
-            } else if (route.name === "My Account") {
-              iconName = focused ? "person-circle" : "person-circle-outline";
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color="#53B175" />
-          },
-          tabBarActiveTintColor: "#53B175",
-          tabBarInactiveTintColor: "#53B175",
-        })}
-        >
-          <Tab.Screen name="Home" 
-            component={HomeStackScreen} 
-            initialParams={{ fb: firebase, addListContext:  handleNewListContext, isRegistered: true }}
-            options={{ 
-              title: 'Home',
-              headerTintColor: '#53B175',
-            }}
-          />
-          <Tab.Screen name="Settings" 
-            component={SettingsStackScreen} 
-            options={{ 
-              title: 'Settings',
-              headerTintColor: '#53B175',
-            }}
-          />
-          <Tab.Screen name="My Account" 
-            component={WelcomeStackScreen} 
-            initialParams={{ existingUser: user, addUserInfo: handleAddUserInfo }}
-            options={{ 
-              title: 'My Account',
-              // headerTitleStyle: styles.tabBarHeaderStyle,
-              headerTintColor: '#53B175',
-                headerLeft: () => (
-                  <Button
-                    onPress={() => alert('This is a button!')}
-                    title="Info"
-                    color="#00cc00"
-                  />
-                ),
-            }}
-          />
-        </Tab.Navigator>
+export default ProfileDrawer;
