3f7751b909191452346d46d8ac8f7dcd14f4bd7d
Meredith Heller <meredith.heller.vabzby@statefarm.com>
2022-03-03 20:25:53 -0500

    added cart screens and stack

---
+++ food-app/Screens/CartStack.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Cart from './Cart.js';
+import PurchaseInfo from './PurchaseInfo.js'
+import ReviewCart from './ReviewCart.js'
+
+const CartStack = createNativeStackNavigator();
+
+const CartStack = ({ navigation, route }) => {
+
+    return (
+        <CartStack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <CartStack.Screen
+                name="CartScreen"
+                component={Cart}
+            />
+            <CartStack.Screen
+                name="PurchaseInfoScreen"
+                component={PurchaseInfo}
+            />
+            <CartStack.Screen
+                name="ReviewCartScreen"
+                component={ReviewCart}
+            /> 
+    </CartStack.Navigator>
+    );
+}
+
+export default CartStack;
---
+++ food-app/Screens/Preferences.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Preferences = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Preferences</Text>
+    </View>
+  )
+}
+export default Preferences;
