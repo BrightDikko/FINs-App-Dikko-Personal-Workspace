70a4e8ca45e6ad02e868cb91922651a93a8145c8
Meredith Heller <meredith.heller.vabzby@statefarm.com>
2022-03-24 20:48:22 -0400

    new code for working with walmart.io affiliate api- getting an error that there is no public key even though i have uploaded it

---
+++ store-api/affiliate_api.py
@@ -0,0 +1,63 @@
+import time
+import os
+from dotenv import load_dotenv
+import base64
+import requests
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+# Load env variables.
+load_dotenv()
+
+consumer_id = os.getenv('WALMART_CONSUMER_ID')
+key_version = os.getenv('WALMART_PROD_KEY_VERSION')
+time_in_ms = str(int(time.time() * 1000))
+
+dict_for_hashing = {
+    'WM_CONSUMER.ID' : consumer_id,
+    'WM_CONSUMER.INTIMESTAMP' : time_in_ms,
+    'WM_SEC.KEY_VERSION' : key_version
+}
+
+# Encode the dict_for_hashing into a hash string - must be in the order below
+hash_str = dict_for_hashing['WM_CONSUMER.ID'] + '\n' + \
+                   dict_for_hashing['WM_CONSUMER.INTIMESTAMP'] + '\n' + \
+                   dict_for_hashing['WM_SEC.KEY_VERSION'] + '\n'
+encodedHashString = hash_str.encode()
+
+# Read the private key from wherever you stored it - mine was in ./id_rsa.
+try:
+    with open('./WM_IO_private_key.pem', 'r') as f:
+        key = RSA.importKey(f.read())
+except IOError as err:
+    print("Could not read private key AAAGH!!", err)
+
+# Get the auth signature required for request header
+hasher = SHA256.new(encodedHashString)
+signer = PKCS1_v1_5.new(key)
+signature = signer.sign(hasher)
+encoded_signature = str(base64.b64encode(signature), 'utf-8')
+
+# Headers for the request to use.
+# This should remain the same for any request to WM IO - Affiliate APIs.
+headers = {
+    'Content-Type' : 'application/json',
+    'WM_CONSUMER.ID' : consumer_id,
+    'WM_CONSUMER.INTIMESTAMP' : time_in_ms,
+    'WM_SEC.AUTH_SIGNATURE' : encoded_signature,
+    'WM_SEC.KEY_VERSION' : key_version,
+    'WM_QOS.CORRELATION_ID' : 'afjksldkfj4r8ojfns',
+    'WM_IFX.CLIENT_TYPE' : 'INTERNAL',
+    'WM_PREVIEW' : 'false',
+    'WM_SHOW_REASON_CODES' : 'ALL',
+}
+
+
+# Attempt to run a test request.
+desired_api_url = 'https://developer.api.walmart.com/api-proxy/service/affil/product/v2/items/4837473'
+parameters = {
+    # 'zip': 46556
+}
+request = requests.get(desired_api_url, params=parameters, headers=headers)
+print(request.json())
